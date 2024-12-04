562491e5f22fbc3c2396aee7b36339b823a14e93
RedaM4 <xxred49xx@gmail.com>
2024-12-04 14:48:53 +0300

    task2 init

---
+++ DSD/assignment2/Task2/Task2.srcs/sources_1/new/Task2.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/04/2024 02:48:21 PM
+// Design Name: 
+// Module Name: Task2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Task2(
+
+    );
+endmodule
